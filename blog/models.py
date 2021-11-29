caefc9ffeab903a7e623c8137e90453ea95bc311
Ernesto Segundo <rsernesto@gmail.com>
2021-11-29 15:35:07 -0500

    Formato al header de la página y creacion de modelos de la sección blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CategoriaPost(models.Model):
+    nombre = models.CharField(max_length=50),
+    disponibilidad = models.BooleanField(default=True),
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Categoría de publicación"
+        verbose_name_plural = "Categorías de publicación"
+    
+    def __str__(self):
+        return self.nombre
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=80)
+    contenido = models.CharField(max_length=999)
+    imagen = models.ImageField(upload_to='blog', null=True, blank=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    categorias = models.ManyToManyField(CategoriaPost)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Publicación"
+        verbose_name_plural = "Publicaciones"
+    
+    def __str__(self):
+        return self.titulo
