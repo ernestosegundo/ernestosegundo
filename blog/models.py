3c5412a0843b7059ca891fb8aad9b7229236c646
Ernesto Segundo <rsernesto@gmail.com>
2021-12-01 20:55:39 -0500

    Actualización del modelo de la aplicación blog, configuración del template blog

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,7 @@ class CategoriaPost(models.Model):
 
 class Post(models.Model):
     titulo = models.CharField(max_length=80)
+    resumen = models.CharField(max_length=250)
     imagen = models.ImageField(upload_to='blog', null=True, blank=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     categorias = models.ManyToManyField(CategoriaPost)
