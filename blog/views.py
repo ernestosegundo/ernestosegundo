05448b408ffe31354e92f17dfb96b88c4a795062
Ernesto Segundo <rsernesto@gmail.com>
2021-12-04 23:35:59 -0500

    Entradas relacionadas

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from blog.models import Post
+from blog.models import Post, CategoriaPost
 
 # Create your views here.
 def blog(request):
@@ -8,5 +8,17 @@ def blog(request):
     return render(request, 'blog/blog.html', {'posts': posts})
 
 def blog_id(request, blog_id):
+    # El post
     post = Post.objects.get(id=blog_id)
-    return render(request, "blog/blogid.html", {"post": post})
+
+    # Las categorias del post
+    categorias_post = post.categorias.all()
+
+    posts = Post.objects.filter(categorias=categorias_post[0])
+    
+    return render(request, "blog/blogid.html", {"post": post, "posts": posts})
+
+def categoria(request, categoria_id):
+    categoria = CategoriaPost.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(request, "blog/categoria.html", {"categoria": categoria, "posts": posts})
