cfad9528dbfc8889315304a04a9baaa8aed045fd
Ernesto Segundo <rsernesto@gmail.com>
2021-12-06 17:52:32 -0500

    Formulario de contacto actualizado con CSS

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class FormularioContacto(forms.Form):
-    nombre = forms.CharField(label="Nombre", required=True)
-    email = forms.EmailField(label="Correo Electrónico", required=True)
-    contenido = forms.CharField(label="Contenido", widget=forms.Textarea, required=False)
+    nombre = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class': 'form-control input-sm'}))
+    email = forms.EmailField(label="Correo Electrónico", required=True, widget=forms.TextInput(attrs={'class': 'form-control input-sm'}))
+    contenido = forms.CharField(label="Contenido", widget=forms.Textarea(attrs={'class': 'form-control input-sm'}), required=False)
--- servicios/models.py
+++ servicios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class CategoriaServicio(models.Model):
+    pass
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=250)
+    disponibilidad = models.BooleanField(default=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now_add=True)
