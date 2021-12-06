c25a80eb0c7a5b585ae54240e35dc4badde93489
Ernesto Segundo <rsernesto@gmail.com>
2021-12-06 11:39:14 -0500

    Formulario de contacto

--- contacto/views.py
+++ contacto/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+
+from contacto.forms import FormularioContacto
 
 # Create your views here.
 def contacto(request):
-    return render(request, 'contacto/contacto.html')
+    formulario_contacto = FormularioContacto()
+
+    if request.method == "POST":
+        formulario_contacto = FormularioContacto(data=request.POST)
+        
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+
+            email = EmailMessage("Mensaje de sitio web Ernesto Segundo", 
+                    "El usuario {} ({}) dice:\n\n {}".format(nombre, email, contenido),
+                    "", ["rsernesto@gmail.com"], reply_to=[email])
+
+            try:
+                email.send()
+
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+
+    return render(request, 'contacto/contacto.html', {"formulario_contacto": formulario_contacto})
--- ernestosegundo/settings.py
+++ ernestosegundo/settings.py
@@ -135,3 +135,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configuración de email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "capitanapiacho@gmail.com"
+EMAIL_HOST_PASSWORD = "543174tit0Mat3Fab9"
