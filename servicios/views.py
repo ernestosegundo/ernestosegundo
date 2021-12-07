90899b9239e3cbb01f944670ba1cb8bbbab6668a
Ernesto Segundo <rsernesto@gmail.com>
2021-12-07 12:23:25 -0500

    Mostrando servicios

--- servicios/views.py
+++ servicios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from servicios.models import CategoriaServicio, Servicio
+
 # Create your views here.
 def servicios(request):
-    return render(request, 'servicios/servicios.html')
+    servicios = Servicio.objects.all()
+
+    return render(request, 'servicios/servicios.html', {"servicios": servicios})
